49689c607295d8d3b3c79d54be7ee991ca2f6faf
Michał Krause <michal.krause01@gmail.com>
2018-12-14 23:26:46 +0100

    Created basic data model. Annotated with JPA tags for Hibernate.

---
+++ src/main/java/com/michalkrause/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,7 @@
+package com.michalkrause.spring5recipeapp.domain;
+
+public enum Difficulty {
+    EASY,
+    MODERATE,
+    HARD
+}
---
+++ src/main/java/com/michalkrause/spring5recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,7 @@
+package com.michalkrause.spring5recipeapp.repositories;
+
+import com.michalkrause.spring5recipeapp.domain.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+}
